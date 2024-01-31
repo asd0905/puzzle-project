3231ecea4308ba462dfec728390b916907a821c7
asd0905 <asd0905@naver.com>
2024-01-31 16:40:33 +0900

    행맨 글자 클릭하면 테스트로 행맨 그림 나오도록 작업

--- src/views/Hangman.vue
+++ src/views/Hangman.vue
@@ -1,5 +1,82 @@
-<script setup lang="ts"></script>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	name: "Hanman",
+	props: {},
+	data() {
+		return {
+			incorrectAttempts: 0,
+			answer: "",
+			alphabet: Array.from({ length: 26 }, (_, index) =>
+				String.fromCharCode("A".charCodeAt(0) + index)
+			),
+		};
+	},
+	methods: {
+		drawHangman() {
+			const canvas = this.$refs.hangmanCanvas as any; // ref로부터 canvas 요소 가져오기
+			if (!canvas) {
+				return;
+			}
+			const ctx = canvas.getContext("2d"); // 옵셔널 체이닝 연산자 사용
+			this.incorrectAttempts++;
+			console.log(this.incorrectAttempts);
+			if (this.incorrectAttempts >= 1) {
+				// Head
+				ctx.beginPath();
+				ctx.arc(100, 40, 20, 0, 2 * Math.PI);
+				ctx.stroke();
+			}
+
+			if (this.incorrectAttempts >= 2) {
+				// Body
+				ctx.moveTo(100, 60);
+				ctx.lineTo(100, 120);
+				ctx.stroke();
+			}
+
+			if (this.incorrectAttempts >= 3) {
+				// Left Arm
+				ctx.moveTo(100, 70);
+				ctx.lineTo(60, 90);
+				ctx.stroke();
+			}
+
+			if (this.incorrectAttempts >= 4) {
+				// Right Arm
+				ctx.moveTo(100, 70);
+				ctx.lineTo(140, 90);
+				ctx.stroke();
+			}
+
+			if (this.incorrectAttempts >= 5) {
+				// Left Leg
+				ctx.moveTo(100, 120);
+				ctx.lineTo(70, 160);
+				ctx.stroke();
+			}
+
+			if (this.incorrectAttempts >= 6) {
+				// Right Leg
+				ctx.moveTo(100, 120);
+				ctx.lineTo(130, 160);
+				ctx.stroke();
+			}
+		},
+	},
+});
+</script>
 
 <template>
-	<main class="mt-7 pt-6">행맨</main>
+	<div>
+		<h1 class="mt-7 pt-6" v-on:click="drawHangman">행맨</h1>
+		<canvas ref="hangmanCanvas" width="300" height="300"></canvas>
+		<input type="text" v-model="answer" />
+		<button class="mx-5" v-for="letter in alphabet" :key="letter">
+			{{ letter }}
+		</button>
+	</div>
 </template>
+
+<style scoped></style>
