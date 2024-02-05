aa78d55c74e915a620a63b06a21af5c66ff9addd
asd0905 <asd0905@naver.com>
2024-02-06 00:40:56 +0900

    행맨 게임 로직 적용

--- src/views/Hangman.vue
+++ src/views/Hangman.vue
@@ -1,16 +1,31 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 
+export interface IalphabetProps {
+	letter: string;
+	isPass: boolean | undefined;
+}
+
 export default defineComponent({
 	name: "Hanman",
 	props: {},
 	data() {
+		const answer = "APPLE";
+		const answerTemp = Array.from(answer).map((d) => "");
+		const alphabetArr = Array.from({ length: 26 }, (_, index) =>
+			String.fromCharCode("A".charCodeAt(0) + index)
+		).map((d) => {
+			return {
+				letter: d,
+				isPass: undefined,
+			};
+		});
+		console.log(alphabetArr);
 		return {
 			incorrectAttempts: 0,
-			answer: "apple",
-			alphabet: Array.from({ length: 26 }, (_, index) =>
-				String.fromCharCode("A".charCodeAt(0) + index)
-			),
+			answer: answer,
+			answerTemp: answerTemp,
+			alphabet: alphabetArr,
 			ctx: {} as any,
 		};
 	},
@@ -27,11 +42,13 @@ export default defineComponent({
 	methods: {
 		drawHangman() {
 			if (this.incorrectAttempts >= 1) {
+				// 긴줄
 				this.ctx.moveTo(20, 0);
 				this.ctx.lineTo(150, 0);
 				this.ctx.stroke();
 			}
 			if (this.incorrectAttempts >= 2) {
+				// 짧은 줄
 				this.ctx.moveTo(100, 0);
 				this.ctx.lineTo(100, 20);
 				this.ctx.stroke();
@@ -80,10 +97,18 @@ export default defineComponent({
 				this.ctx.stroke();
 			}
 		},
-		clickLetter(letter: string) {
-			console.log(letter);
-			this.incorrectAttempts++;
-			this.drawHangman();
+		checkAnswer(data: IalphabetProps) {
+			[...this.answer].filter((d, i) => {
+				if (data.letter === d) {
+					data.isPass = true;
+					this.answerTemp[i] = d;
+				}
+			});
+			if (!data.isPass) {
+				data.isPass = false;
+				this.incorrectAttempts++;
+				this.drawHangman();
+			}
 		},
 	},
 });
@@ -101,22 +126,25 @@ export default defineComponent({
 		<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="3">
 			<line x1="0" y1="0" x2="100%" y2="0" stroke="black" stroke-width="3" />
 		</svg>
-		<div class="input-box mb-[50px]">
+		<div class="input-box mb-8 mt-5">
 			<input
 				type="text"
 				class="answer-input"
 				disabled
-				v-for="(a, index) in answer"
+				v-for="(a, index) in answerTemp"
+				v-bind:value="a"
 				:key="index"
 			/>
 		</div>
 		<div class="letter-box">
 			<button
-				v-for="letter in alphabet"
-				:key="letter"
-				v-on:click="clickLetter(letter)"
+				v-for="d in alphabet"
+				:key="d.letter"
+				:class="[{ fail: d.isPass === false }, { pass: d.isPass }]"
+				:disabled="d.isPass === false || d.isPass"
+				v-on:click="checkAnswer(d)"
 			>
-				{{ letter }}
+				{{ d.letter }}
 			</button>
 		</div>
 	</div>
@@ -129,7 +157,7 @@ export default defineComponent({
 	justify-content: center;
 }
 .answer-input {
-	margin: 0 10px;
+	margin: 0 5px;
 	border-bottom: 1px solid black;
 	max-width: 8%;
 	text-align: center;
@@ -147,6 +175,12 @@ export default defineComponent({
 		padding: 14px 20px;
 		border-radius: 10px;
 		&:hover {
+			background-color: hsla(160, 100%, 37%, 1);
+		}
+		&.fail {
+			background-color: gray;
+		}
+		&.pass {
 			background-color: hsla(160, 100%, 37%, 1);
 		}
 	}
